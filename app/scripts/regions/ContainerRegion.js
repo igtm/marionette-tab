4c4fc61d9c84cb20d1c5479aa33ec37e39617a10
igtm <tomo14916253@gmail.com>
2014-05-17 16:33:29 +0900

    collection・Layout・ViewをContainerRegionに保持。ページ戻ってきても、元通り表示される。それぞれ初回のみインスタンス化される。

--- app/scripts/regions/ContainerRegion.js
+++ app/scripts/regions/ContainerRegion.js
@@ -17,27 +17,47 @@ function( Backbone, Communicator, FirstTabLayout, Lists, HeaderItemView, Content
             Communicator.command.setHandler('firstTab',this.firstTab,this);
             Communicator.command.setHandler('secondTab',this.secondTab,this);
             Communicator.command.setHandler('thirdTab',this.thirdTab,this);
-            this.firstTab();
+
+            // Layout ごとのコレクション
+            this.firstCollection = new Lists();
+            this.secondCollection = new Lists();
+            this.thirdCollection = new Lists();
         },
         el: '#container',
 
+        //それぞれ３枚のViewを[this.--TLayout],[this.--HItemView],[this.--CCompositeView]に保存
         firstTab: function(){
-                this.first = new FirstTabLayout();
-                this.show(this.first);
-                this.first.header.show(new HeaderItemView());
-                this.first.content.show(new ContentCompositeView({collection: new Lists()}));
+            if(!this.FTLayout) { //初回のみ３枚のViewを作成
+                console.log('作成！！');
+                this.FTLayout = new FirstTabLayout();
+                this.FHItemView = new HeaderItemView();
+                this.FCCompositeView = new ContentCompositeView(({collection: this.firstCollection}));
+            }
+                this.show(this.FTLayout,{preventClose: true});
+                this.FTLayout.header.show(this.FHItemView,{preventClose: true});
+                this.FTLayout.content.show(this.FCCompositeView,{preventClose: true});
         },
         secondTab: function(){
-                this.second = new FirstTabLayout();
-                this.show(this.second);
-                this.second.header.show(new HeaderItemView());
-                this.second.content.show(new ContentCompositeView({collection: new Lists()}));
+            if(!this.STLayout) { //初回のみ３枚のViewを作成
+                console.log('作成！！');
+                this.STLayout = new FirstTabLayout();
+                this.SHItemView = new HeaderItemView();
+                this.SCCompositeView = new ContentCompositeView(({collection: this.secondCollection}));
+            }
+                this.show(this.STLayout,{preventClose: true});
+                this.STLayout.header.show(this.SHItemView,{preventClose: true});
+                this.STLayout.content.show(this.SCCompositeView,{preventClose: true});
         },
         thirdTab: function(){
-                this.third = new FirstTabLayout();
-                this.show(this.third);
-                this.third.header.show(new HeaderItemView());
-                this.third.content.show(new ContentCompositeView({collection: new Lists()}));
+            if(!this.TTLayout) { //初回のみ３枚のViewを作成
+                console.log('作成！！');
+                this.TTLayout = new FirstTabLayout();
+                this.THItemView = new HeaderItemView();
+                this.TCCompositeView = new ContentCompositeView(({collection: this.thirdCollection}));
+            }
+                this.show(this.TTLayout,{preventClose: true});
+                this.TTLayout.header.show(this.THItemView,{preventClose: true});
+                this.TTLayout.content.show(this.TCCompositeView,{preventClose: true});
         }
 	});
 
